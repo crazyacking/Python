3a2fbcdefe73a38ac5fd0f42e439b6f5b15730ce
crazyacking <11301655@qq.com>
2016-01-14 17:21:06 +0800

    Changes to be committed: 	modified:   PythonLearn/.idea/workspace.xml 	modified:   PythonLearn/main.py 	renamed:    "python\345\256\236\347\216\260\346\226\207\344\273\266\350\275\254\345\202\250/backup_version1.py" -> "python\345\256\236\347\216\260\346\226\207\344\273\266\345\244\207\344\273\275/backup_version1.py" 	renamed:    "python\345\256\236\347\216\260\346\226\207\344\273\266\350\275\254\345\202\250/backup_version2.py" -> "python\345\256\236\347\216\260\346\226\207\344\273\266\345\244\207\344\273\275/backup_version2.py"

--- PythonLearn/main.py
+++ PythonLearn/main.py
@@ -1058,3 +1058,22 @@
 # else:
 # 	print("Backup FAILED!")
 
+
+import os
+import time
+
+source=['/home/crazyacking/swaroop/byte/','/home/crazyacking/swaroop/bin/']
+target_dir='/home/crazyacking/swaroop/bak/'
+folder_name=time.strftime("%Y%m%d")
+create_folder_command='mkdir '+target_dir+folder_name
+if not os.path.exists(target_dir+folder_name):
+	os.mkdir(target_dir+folder_name)
+	print("Successful create folder:"+target_dir+folder_name+".")
+#	os.system(create_folder_command)
+target_dir+=folder_name+'/'
+backup_name=target_dir+time.strftime("%H_%M_%S")+'.zip'
+zip_command="zip -qr '%s' %s"%(backup_name,' '.join(source))
+if os.system(zip_command)==0:
+	print("Successful backup to:"+backup_name)
+else:
+	print("Backup FAILED!")
--- python实现文件备份/backup_version2.py
+++ python实现文件备份/backup_version2.py
@@ -6,11 +6,13 @@ target_dir='/home/crazyacking/swaroop/bak/'
 folder_name=time.strftime("%Y%m%d")
 create_folder_command='mkdir '+target_dir+folder_name
 if not os.path.exists(target_dir+folder_name):
-	os.system(create_folder_command)
+	os.mkdir(target_dir+folder_name)
+	print("Successful create folder:"+target_dir+folder_name+".")
+#	os.system(create_folder_command)
 target_dir+=folder_name+'/'
 backup_name=target_dir+time.strftime("%H_%M_%S")+'.zip'
 zip_command="zip -qr '%s' %s"%(backup_name,' '.join(source))
 if os.system(zip_command)==0:
-	print("Successful backup to",backup_name)
+	print("Successful backup to:"+backup_name)
 else:
 	print("Backup FAILED!")
